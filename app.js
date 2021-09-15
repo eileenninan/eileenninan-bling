c2faf127bc78be1116e44acb2794c463d12fa23d
Eileen Ninan <eileenninan@Eileens-MacBook-Pro.local>
2021-09-15 12:35:49 +0530

    Added Font

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ Your code goes in this file
 --------------------------------------------------------------
 */
 let root = document.querySelector("#root");
+
+root.style.cssText="font-family: 'Montserrat', sans-serif;"
