3527161cd6a4dc5ac44f4d76fd093192c341d6bf
Eileen Ninan <eileenninan@Eileens-MacBook-Pro.local>
2021-09-15 12:58:44 +0530

    Added Heading and styled the heading.

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ Your code goes in this file
 let root = document.querySelector("#root");
 
 root.style.cssText="font-family: 'Montserrat', sans-serif;"
+
+const heading = document.createElement("h1");
+heading.innerText = "BLING!";
+heading.style.textAlign = "center";
+heading.style.fontWeight = "bolder";
+heading.style.fontSize = "35px";
+heading.style.padding = "1em";
+heading.style.color = "#2d2d2d";
+heading.style.textShadow = "4.9px 4.9px #f39c12";
+root.appendChild(heading);
